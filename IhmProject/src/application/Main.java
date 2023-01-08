1bfa8989bc32d40388976c91751519fd3af7db76
MariamAboujenane <mariamaboujenane2912@gmail.com>
2023-01-08 22:21:12 +0100

    mariam

--- IhmProject/src/application/Main.java
+++ IhmProject/src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 	
 	public void start(Stage primaryStage) {
 		try {
-			Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("View/AccountClient.fxml"));
+			Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("View/ClientHistory.fxml"));
 			Scene scene = new Scene(parent);
 		
 			
