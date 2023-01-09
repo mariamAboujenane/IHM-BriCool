267fd85bda7f0803f57b64a844b05d8c58867cd1
MariamAboujenane <mariamaboujenane2912@gmail.com>
2023-01-09 20:51:44 +0100

    mariam

--- IhmProject/src/Model/MainModel.java
+++ IhmProject/src/Model/MainModel.java
@@ -6,33 +6,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-//shadia
-
-//tasnime
+// mariam 
 public class MainModel {
 	private Connection myConnection;
 	Statement statement;
 	ResultSet resultat;
 
-
-	
 	public MainModel() {
 
-		String hostname = "sql6.freesqldatabase.com";
-	    String port = "3306";
-	    String database = "sql6586993";
-	    String username = "sql6586993";
-	    String password = "2vHvuWvuWZ";
-
-	    // Création de la chaîne de connexion à la base de données
-	    String jdbcUrl = String.format("jdbc:mysql://%s:%s/%s?useSSL=false", hostname, port, database);
-
-	    // Connexion à la base de données
-	    try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
-	      System.out.println("Connecté à la base de données");
-	    } catch (SQLException e) {
-	      System.out.println("Erreur de connexion à la base de données : " + e.getMessage());
-	    }
-	 }
+		try {
+			myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bricool", "root", "");
+			statement = myConnection.createStatement();
+			myConnection.close();
+			System.out.println("connected");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
-
