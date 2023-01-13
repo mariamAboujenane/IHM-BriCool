2c5152ff95e8bd22af2f50da785ec03e4c2259c3
SAHELBouchra <bouchrasahel43@gmail.com>
2023-01-13 15:22:26 +0100

    buuuushra

---
+++ IhmProject/src/Controller/AccountProvider.java
@@ -0,0 +1,46 @@
+package src.Controller;
+
+import java.io.IOException;
+
+import javafx.event.*;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class AccountProvider {
+	
+	
+	   @FXML
+	    private Button back;
+
+	    @FXML
+	    void Back_To_choose(ActionEvent event) {
+	    	
+	    	try {
+				Parent parent;
+				parent = FXMLLoader.load(getClass().getClassLoader().getResource("src/View/clientorprovider.fxml"));
+				
+				Scene scene = new Scene(parent);
+				
+				Stage  primaryStage = new Stage();
+				primaryStage.setScene(scene);
+				primaryStage.show();
+				
+				Stage stage = (Stage) back.getScene().getWindow();
+			    // do what you have to do
+			  stage.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	
+
+	    }
+
+}
--- IhmProject/src/Controller/usercontroller.java
+++ IhmProject/src/Controller/usercontroller.java
@@ -1,6 +1,8 @@
 package src.Controller;
 import javafx.event.ActionEvent;
 import src.View.*;
+
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,6 +14,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.*;
 import java.sql.Connection;
@@ -22,6 +25,10 @@ import java.sql.Statement;
 
 public class usercontroller {
     private ResourceBundle resources;
+    
+    @FXML
+    private Button back;
+    
     private URL location;
     @FXML
     private Button create;
@@ -35,6 +42,31 @@ public class usercontroller {
     private  TextField txtuser;
  	
    //zineb
+    
+    
+    @FXML
+    void Back_To_choose(ActionEvent event) {
+    	
+    	try {
+			Parent parent;
+			parent = FXMLLoader.load(getClass().getClassLoader().getResource("src/View/clientorprovider.fxml"));
+			
+			Scene scene = new Scene(parent);
+			
+			Stage  primaryStage = new Stage();
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			Stage stage = (Stage) back.getScene().getWindow();
+		    // do what you have to do
+		  stage.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    }
    
 	@FXML
 	public void createOnAction(ActionEvent e) {
@@ -62,7 +94,10 @@ public class usercontroller {
       			  alert.showAndWait();
               }
        
-              
+             
+             
+             
+            
      		
 	}
 
