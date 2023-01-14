50093ee1d848f0d7e73931f74db5bfdbd9b33b27
SAHELBouchra <bouchrasahel43@gmail.com>
2023-01-14 14:59:18 +0100

    pro

--- IhmProject/src/Controller/AccountProvider.java
+++ IhmProject/src/Controller/AccountProvider.java
@@ -1,20 +1,30 @@
 package src.Controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.*;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
-public class AccountProvider {
+public class AccountProvider implements Initializable{
 	
 	
+	 @FXML
+	    private ChoiceBox<String> Mychoicebox; 
+	 
+	 
+	   private String [] specialty = {"CARPENTER","PLUMBER","CLEANER","TAILOR HAIR STYLIST","ELECTRICIAN"};
+	   
 	   @FXML
 	    private Button back;
 
@@ -42,5 +52,11 @@ public class AccountProvider {
 	    	
 
 	    }
+
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+			 Mychoicebox.getItems().addAll(specialty);
+			
+		}
 
 }
