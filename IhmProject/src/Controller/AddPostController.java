33a395095e059605eb6d41f0bc28f299cf436329
Shadia AIT EL CADI <89936910+ShadiaAITELCADI@users.noreply.github.com>
2023-01-14 22:41:56 +0100

    heeeeeeeu

--- IhmProject/src/Controller/AddPostController.java
+++ IhmProject/src/Controller/AddPostController.java
@@ -1,14 +1,21 @@
 package src.Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.TransferMode;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -16,6 +23,7 @@ import javafx.scene.image.ImageView;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 
@@ -27,6 +35,7 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Optional;
 import java.util.TimeZone;
 
 import javafx.event.ActionEvent;
@@ -53,6 +62,165 @@ public class AddPostController {
     
     private FileInputStream postImage;
     List<File> files;
+    private DialogPane dialog;
+	   @FXML
+	    private Button btn_information;
+	   
+	  
+	   @FXML
+	    private Button btn_notification;
+	   @FXML
+	    private Button back;
+
+	    @FXML
+	    void Back_To_choose(ActionEvent event) {
+	    	Parent parent;
+			try {
+				parent = FXMLLoader.load(getClass().getClassLoader().getResource("src/View/SProfile.fxml"));
+				Scene scene = new Scene(parent);
+				
+				Stage  primaryStage = new Stage();
+				primaryStage.setScene(scene);
+				primaryStage.show();
+				Stage stage1 = (Stage) back.getScene().getWindow();
+			    stage1.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+	   @FXML
+	    void gonotification(ActionEvent event) {
+     	
+			try {
+				Parent parent;
+				parent = FXMLLoader.load(getClass().getClassLoader().getResource("src/View/Notification.fxml"));
+				
+				Scene scene = new Scene(parent);
+				
+				Stage  primaryStage = new Stage();
+				primaryStage.setScene(scene);
+				primaryStage.show();
+				
+				Stage stage = (Stage) btn_notification.getScene().getWindow();
+			    // do what you have to do
+			  stage.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	
+			
+	    }
+	    @FXML
+	    void goinformation(ActionEvent event) {
+	    	try {
+				Parent parent;
+				parent = FXMLLoader.load(getClass().getClassLoader().getResource("src/View/information.fxml"));
+				
+				Scene scene1 = new Scene(parent);
+				
+				Stage  primaryStage1 = new Stage();
+				primaryStage1.setScene(scene1);
+				primaryStage1.show();
+				
+				Stage stage1 = (Stage) btn_information.getScene().getWindow();
+			    // do what you have to do
+			  stage1.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	
+
+	    }
+	    
+	  
+	    @FXML
+	    void gohistory(ActionEvent event) {
+	    	try {
+				Parent parent;
+				parent = FXMLLoader.load(getClass().getClassLoader().getResource("src/View/ClientHistory.fxml"));
+				
+				Scene scene1 = new Scene(parent);
+				
+				Stage  primaryStage1 = new Stage();
+				primaryStage1.setScene(scene1);
+				primaryStage1.show();
+				
+				Stage stage1 = (Stage) btn_information.getScene().getWindow();
+			    // do what you have to do
+			     stage1.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	
+	    }
+	    @FXML
+	    void singnout(ActionEvent event) {
+	    	
+	    	try {
+	    		
+	    		Alert alert = new Alert(AlertType.CONFIRMATION );
+	    		alert.setHeaderText(null);
+	    		alert.setContentText("Are you sure that you want to log out !");
+	    		alert.initModality(Modality.NONE);
+	    		dialog= alert.getDialogPane();  
+			dialog.getStylesheets().add(getClass().getResource("style2.css").toString());
+			 dialog.getStyleClass().add("dialog");
+  			Optional<ButtonType> result = alert.showAndWait();
+  			 if(result.isEmpty()) {
+  				System.out.print("Alert closed ");
+  		     }else if(result.get()==ButtonType.OK) {
+  	
+  		    	Parent parent;
+				parent = FXMLLoader.load(getClass().getClassLoader().getResource("src/View/login.fxml"));
+				
+				Scene scene1 = new Scene(parent);
+				
+				Stage  primaryStage1 = new Stage();
+				primaryStage1.setScene(scene1);
+				primaryStage1.show();
+				Stage stage1 = (Stage) btn_information.getScene().getWindow();
+			     stage1.close(); 
+  		     }else if(result.get()==ButtonType.OK) {  
+  		    	System.out.print("Alert closed "); 
+  		     }
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    
+	    }
+	    @FXML
+	    void goeditprofil(ActionEvent event) {
+	    	try {
+				Parent parent;
+				parent = FXMLLoader.load(getClass().getClassLoader().getResource("src/View/EditProfileUser.fxml"));
+				
+				Scene scene1 = new Scene(parent);
+				
+				Stage  primaryStage1 = new Stage();
+				primaryStage1.setScene(scene1);
+				primaryStage1.show();
+				
+				Stage stage1 = (Stage) btn_information.getScene().getWindow();
+			    // do what you have to do
+			     stage1.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    
+	    }
+	
+
+	
+
     @FXML
     void handleDragOver(DragEvent event) {  //we use drag event if we went to drag something from another application and drop it in our fxml file
         if(event.getDragboard().hasFiles()) { //we did a condition here to accept just files .
