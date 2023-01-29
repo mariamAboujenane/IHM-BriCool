57aca89807c0742d29674742af554a8c44a97aa7
Zineb M'HIFED <121026257+zineb-212@users.noreply.github.com>
2023-01-29 01:59:15 +0100

    Update AddPhotoController.java

--- IhmProject/src/Controller/AddPhotoController.java
+++ IhmProject/src/Controller/AddPhotoController.java
@@ -69,15 +69,21 @@ public class AddPhotoController {
 	    void cancelOnAction(ActionEvent event) {
 	    	Parent parent;
 			try {
+				// Load the FXML file using FXMLLoader
 				parent = FXMLLoader.load(getClass().getClassLoader().getResource("src/View/login.fxml"));
+				// Create a new Scene with the loaded FXML file as the root node
 				Scene scene = new Scene(parent);
-				
 				Stage  primaryStage = new Stage();
+				// Set the Scene on the Stage
 				primaryStage.setScene(scene);
-				 Image image = new Image("src/View/icons/logo3.png");	 
-					primaryStage.getIcons().add(image);
-					primaryStage.setTitle("BriCOOL");
+				// Create a new Image object 
+				Image image = new Image("src/View/icons/logo3.png");	 
+			    // Add the image to the icon list of the primaryStage
+				primaryStage.getIcons().add(image);
+			    // Set the title of the primaryStage
+				primaryStage.setTitle("BriCOOL");
 				primaryStage.show();
+				//Get the current scene and close it
 				Stage stage1 = (Stage) cancel.getScene().getWindow();
 			    stage1.close();
 			} catch (IOException e) {
@@ -86,11 +92,13 @@ public class AddPhotoController {
 
 	    }
 	
-
+   //method that handles the drag-and-drop event of a file over a specific area
 
     @FXML
     void handleDragOver(DragEvent event) { 
+    	//checks if the event contains any files
         if(event.getDragboard().hasFiles()) { 
+        //accepts the transfer mode of the files
     	event.acceptTransferModes(TransferMode.ANY); 
     	addButton.setDisable(false);
 
@@ -99,10 +107,15 @@ public class AddPhotoController {
     
     @FXML
     void handledrop(DragEvent event) throws FileNotFoundException {
+     // Retrieve the files from the Dragboard
      files =event.getDragboard().getFiles();
+     // Retrieve the path of the first file dropped
      path=(files.get(0)).getAbsolutePath();
+     // Create a new FileInputStream using the first file
      postImage =new FileInputStream(files.get(0));
+     // Create a new Image object using the FileInputStream
      image=new Image(postImage);
+     // Set the Image object on the ImageView
      imageView.setImage(image);
      draganddrop.setVisible(false);
      uploadImage.setVisible(false);
@@ -112,20 +125,26 @@ public class AddPhotoController {
     void backOnAction(ActionEvent event) {
     	Parent parent1 = null;
 		try {
+			// Load the FXML file using FXMLLoader
 			parent1 = FXMLLoader.load(getClass().getClassLoader().getResource("src/View/AccountProvider.fxml"));
 			
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
+		// Create a new Scene with the loaded FXML file as the root node
 		Scene scene = new Scene(parent1);
 		Stage  primaryStage = new Stage();
+		// Set the Scene on the Stage
 		primaryStage.setScene(scene);
-		 Image image = new Image("src/View/icons/logo3.png");	 
-			primaryStage.getIcons().add(image);
-			primaryStage.setTitle("BriCOOL");
-		primaryStage.show(); 
+		Image image = new Image("src/View/icons/logo3.png");	 
+	    // Set the Scene on the Stage
+		primaryStage.getIcons().add(image);
+	    // Set the title of the primaryStage
+		primaryStage.setTitle("BriCOOL");
+		primaryStage.show();
+		//get the current scene and close it
 		Stage stage1 = (Stage) Back.getScene().getWindow();
-	     stage1.close();
+	    stage1.close();
 
 
 
@@ -135,28 +154,33 @@ public class AddPhotoController {
  
     @FXML
     void addAction(ActionEvent event) throws FileNotFoundException {	
-    	
+        // Establish a connection to the MySQL database
     	DatabaseConnection connectNow=new  DatabaseConnection();
 		Connection connect = connectNow.getConnection();
 		String addPhotoQuery ="update service_provider set photo = ? where idprovider= '"+AccountProvider.id+"'";
 		try {
+  		    // Create a FileInputStream to read the contents of the image file
             FileInputStream fis = new FileInputStream(path);
-
+            //write the contents of the file into a byte array
 			ByteArrayOutputStream baos= new ByteArrayOutputStream();
-			 byte[] buffer = new byte[1024];
-	            int read;
-	            try {
-					while ((read = fis.read(buffer)) != -1) {
-					    baos.write(buffer, 0, read);
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			byte[] buffer = new byte[1024];
+            //Variable to store the number of bytes read from the file
+	        int read;
+	        try {
+            // Read the contents of the file in chunks and write them to the ByteArrayOutputStream until the entire file has been read
+			while ((read = fis.read(buffer)) != -1) {
+			   baos.write(buffer, 0, read);
+			}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	            byte[] imageBytes = baos.toByteArray();
-
+                // Create a prepared statement to execute the SQL query
 	   	 	    PreparedStatement st = connect.prepareStatement(addPhotoQuery);
-	   	        st.setBytes(1, imageBytes);
+	   	        //setting the image 
+	   	 	    st.setBytes(1, imageBytes);
 				st.execute();
+				//confirmation alert
 				   Alert alert = new Alert(Alert.AlertType.ERROR);
 				    alert.setTitle("Confirmation");
 				    alert.setHeaderText("Photo added succesfully !");
